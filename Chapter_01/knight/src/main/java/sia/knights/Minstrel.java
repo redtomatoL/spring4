d1fc6db6077f1b07819dc4a31fe5b597c80ac2c5
liujingming <redtomatoL@163.com>
2019-03-15 15:42:26 +0800

    first commit

---
+++ Chapter_01/knight/src/main/java/sia/knights/Minstrel.java
@@ -0,0 +1,25 @@
+package sia.knights;
+
+import java.io.PrintStream;
+
+/**
+ *  我是个吟唱诗人
+ */
+public class Minstrel {
+
+  private PrintStream stream;
+  
+  public Minstrel(PrintStream stream) {
+    this.stream = stream;
+  }
+
+  public void singBeforeQuest() {
+    stream.println("Fa la la, the knight is so brave!");
+  }
+
+  public void singAfterQuest() {
+    stream.println("Tee hee hee, the brave knight " +
+    		"did embark on a quest!");
+  }
+
+}
